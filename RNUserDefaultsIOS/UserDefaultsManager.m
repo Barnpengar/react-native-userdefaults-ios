4403e4bb9b9da8a21024fac91e69e2fe023aa421
anback <anders.back@me.com>
2016-12-16 11:27:13 +0100

    hard coded group.gimi.dev

--- RNUserDefaultsIOS/UserDefaultsManager.m
+++ RNUserDefaultsIOS/UserDefaultsManager.m
@@ -12,42 +12,35 @@
 
 + (void)setObject:(id)object forKey:(NSString *)key {
     
-    [[NSUserDefaults standardUserDefaults] setObject:object forKey:key];
+    return [[[NSUserDefaults alloc] initWithSuiteName:@"group.gimi.dev"] setObject:object forKey:key];
 }
 
 + (void)setBool:(bool)value forKey:(NSString *)key {
-    
-    [[NSUserDefaults standardUserDefaults] setBool:value forKey:key];
+    return [[[NSUserDefaults alloc] initWithSuiteName:@"group.gimi.dev"] setBool:value forKey:key];
 }
 
 + (void)removeObjectForKey:(NSString *)key {
-    
-    [[NSUserDefaults standardUserDefaults] removeObjectForKey:key];
+    return [[[NSUserDefaults alloc] initWithSuiteName:@"group.gimi.dev"] removeObjectForKey:key];
 }
 
 + (NSArray *)arrayForKey:(NSString *)key {
-    
-    return [[NSUserDefaults standardUserDefaults] arrayForKey:key];
+    return [[[NSUserDefaults alloc] initWithSuiteName:@"group.gimi.dev"] arrayForKey:key];
 }
 
 + (NSString *)stringForKey:(NSString *)key {
-    
-    return [[NSUserDefaults standardUserDefaults] stringForKey:key];
+    return [[[NSUserDefaults alloc] initWithSuiteName:@"group.gimi.dev"] stringForKey:key];
 }
 
 + (id)objectForKey:(NSString *)key {
-    
-    return [[NSUserDefaults standardUserDefaults] objectForKey:key];
+    return [[[NSUserDefaults alloc] initWithSuiteName:@"group.gimi.dev"] objectForKey:key];
 }
 
 + (bool)boolForKey:(NSString *)key {
-    
-    return [[NSUserDefaults standardUserDefaults] boolForKey:key];
+    return [[[NSUserDefaults alloc] initWithSuiteName:@"group.gimi.dev"] boolForKey:key];
 }
 
 + (NSArray *)getAllKeys:(NSString *)key {
-    
-    return [[[NSUserDefaults standardUserDefaults] dictionaryRepresentation] allKeys];
+    return [[[[NSUserDefaults alloc] initWithSuiteName:@"group.gimi.dev"] dictionaryRepresentation] allKeys];
 }
 
 @end
